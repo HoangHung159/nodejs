5ce2b1f54bec87a56de389b19a1047250ab607b4
Hung Hoang <Hunghoang159bmt@gmail.com>
2022-11-02 16:23:06 +0700

    30. [CRUD] Update course

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ const path = require('path');
 const morgan = require('morgan');
 const express = require('express');
 const { engine } = require('express-handlebars');
-const app = express();
-const port = 3000;
+const methodOverride = require('method-override');
 
 const route = require('./routes');
+const db = require('./config/db');
+
+// connect to db
+db.connect();
+
+const app = express();
+const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -16,6 +22,8 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 // http loger
 app.use(morgan('combined'));
 
@@ -24,14 +32,17 @@ app.engine(
     'hbs',
     engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     }),
 );
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // routes init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`app listening on port ${port}`);
 });
