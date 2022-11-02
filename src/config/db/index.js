3c00002a383df3ab40ecefea22561c49446a87f9
Hung Hoang <Hunghoang159bmt@gmail.com>
2022-11-02 17:21:57 +0700

    31. [CRUD] Delete course

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/f8_edu_dev');
+        await mongoose.connect('mongodb://localhost:27017/f8_edu_dev', {
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // useCreateIndex: true,
+        });
         console.log('succes');
     } catch (error) {
         console.log('fail');
