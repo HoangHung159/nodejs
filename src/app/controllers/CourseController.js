4e4d3a2e230daa8710f05ab311d2b662e391e2ce
Hung Hoang <Hunghoang159bmt@gmail.com>
2022-11-03 21:11:57 +0700

    32. Soft delete?

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -21,12 +21,11 @@ class CourseController {
     // [POST] /search/store
     store(req, res, next) {
         // res.json(req.body)
-        const formData = req.body;
-        formData.img = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
-        const course = new Course(formData);
+        req.body.img = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+        const course = new Course(req.body);
         course
             .save()
-            .then(() => res.redirect('/'))
+            .then(() => res.redirect('/me/stored/courses'))
             .catch((error) => {});
     }
 
@@ -48,9 +47,23 @@ class CourseController {
             .catch(next);
     }
 
-    // [DELETE] /search/:id
+    // [DELETE] /courses/:id
     destroy(req, res, next) {
+        Course.delete({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    // [DELETE] /courses/:id/force
+    forceDestroy(req, res, next) {
         Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next);
+    }
+
+    // [PATCH] /courses/:id/restore
+    restore(req, res, next) {
+        Course.restore({ _id: req.params.id })
             .then(() => res.redirect('back'))
             .catch(next);
     }
